572e39fe20353feeaca339e6d5ceedd744011d14
Dmitriy Dyomin <dmitrodem@gmail.com>
2023-12-27 15:24:03 +0300

    WIP

--- sim/tb_axi_udp_rx.py
+++ sim/tb_axi_udp_rx.py
@@ -2,15 +2,13 @@
 
 import sys
 import cocotb
-from pytun import TunTapDevice, IFF_TAP, IFF_NO_PI
+
 from cocotb.clock import Clock
-from cocotb.triggers import RisingEdge
+from cocotb.triggers import RisingEdge, FallingEdge
 from cocotb.types import LogicArray
 import codecs
 import struct
 import fcntl
-
-# eth0 = TunTapDevice(name = "sim0", flags = IFF_TAP | IFF_NO_PI)
 
 def create_tap(name = "tap0")
     TUNSETIFF = 0x400454ca
@@ -18,7 +16,7 @@ def create_tap(name = "tap0")
     IFF_TUN = 0x0001
     IFF_TAP = 0x0002
     IFF_NO_PI = 0x1000
-    dev = open('/dev/net/tun', 'r+b')
+    dev = open('/dev/net/tun', 'r+b', buffering = 0)
     ifr = struct.pack('16sH', name, IFF_TAP | IFF_NO_PI)
     fcntl.ioctl(tun, TUNSETIFF, ifr)
     return dev
@@ -39,14 +37,14 @@ def lookahead(iterable):
     # Report the last value.
     yield last, True
 
-    
+
 async def listener(clk, tdata, tlast, tvalid, tready):
     npackets = 0
     tready.value = 1
     while True:
         data = eth0.read(eth0.mtu)
         print(f"<<< {data.hex()}")
-        for i, ch in enumerate(data):            
+        for i, ch in enumerate(data):
             tlast.value = 0 if i < len(data)-1 else 1
             tdata.value = ch
             tvalid.value = 1
@@ -62,16 +60,16 @@ async def listener(clk, tdata, tlast, tvalid, tready):
 async def sender(clk, tdata, tlast, tvalid, tready):
     packet = bytearray()
     tready.value = 1
-    while True:        
+    while True:
         await RisingEdge(clk)
         if (tvalid.value == 1):
-            packet.append(tdata.value.integer)            
+            packet.append(tdata.value.integer)
             if (tlast.value == 1):
                 # print(f">>>> {packet.hex()}")
                 eth0.write(bytes(packet))
                 packet = bytearray()
 
-async def send_packet(pkt, clk, tdata, tlast, tvalid, tready):    
+async def send_packet(pkt, clk, tdata, tlast, tvalid, tready):
     for xdata, xlast in lookahead(pkt):
         await RisingEdge(clk)
         tdata.value = xdata
@@ -94,11 +92,11 @@ async def receive_packet(clk, tdata, tlast, tvalid, tready):
                 break
     tready.value = 0
     return bytes(pkt)
-                
+
 @cocotb.test()
 async def tb_axi_udp_rx(dut):
     dut.aresetn.value = 0;
-    
+
     clock = Clock(dut.clk, 10, units="us")
     cocotb.start_soon(clock.start(start_high=False))
 
@@ -113,7 +111,7 @@ async def tb_axi_udp_rx(dut):
 
     dut.aresetn.value = 1;
     await RisingEdge(dut.clk)
-    
+
     # cocotb.start_soon(sender(dut.clk, dut.m_axis_tdata, dut.m_axis_tlast, dut.m_axis_tvalid, dut.m_axis_tready))
     # await listener(dut.clk, dut.s_axis_tdata, dut.s_axis_tlast, dut.s_axis_tvalid, dut.s_axis_tready)
     tpkt = codecs.decode("ffffffffffffbe4f933c359f08060001080006040001be4f933c359fc0a80601000000000000c0a80602", "hex")
